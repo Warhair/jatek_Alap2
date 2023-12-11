a1ee49c6edea3a435432390cb7bd8906045c0885
Warhair <tegzesmarton@gmail.com>
2023-12-11 11:37:01 +0100

    mentes és betoltes

--- src/jatek/Felszereles.java
+++ src/jatek/Felszereles.java
@@ -4,7 +4,15 @@
  */
 package jatek;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -49,6 +57,31 @@ public class Felszereles {
     @Override
     public String toString() {
         return "Felszereles{" + "leiras=" + leiras + ", nev=" + nev + ", id=" + id + '}';
+    }
+    public void mentes(Karakter karakterek){
+           try(ObjectOutputStream objKi= new ObjectOutputStream(new FileOutputStream("karakterek.bin"))){
+           
+            objKi.writeObject(karakterek);
+            
+           
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void betoltes(Karakter karakterek){
+        try(ObjectInputStream objBe= new ObjectInputStream(new FileInputStream("karakterek.bin"))){
+            /* a kiírásnak megfelelő típusú objektum a megfelelő sorrendben
+               visszaolvasható, visszaállítható, 
+               a kiírásnak megfelelő object kerül beolvasása: Karakter[]
+            */
+            karakterek = (Karakter) objBe.readObject();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     
